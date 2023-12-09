4d58ca08941ca7fee9cf4f61ea82dedc4c520e31
user <yannlefranc330@gmail.com>
2023-12-09 14:19:26 +0100

    java soutient au corp medical

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int medical = 20;
+        int medical2 = 18;
+        if (medical > medical2) {
+            System.out.println("Un grand soutient aux perssonel medical");
+        }
+    }
+}
